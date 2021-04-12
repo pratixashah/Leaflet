667d67444f4578a03cc32c11a0722e35dd02168c
pratixashah <shah.pratixa@gmail.com>
2021-04-12 14:47:43 -0700

    Added Legends

--- Leaflet-Step-1/js/app.js
+++ Leaflet-Step-1/js/app.js
@@ -2,13 +2,13 @@
     
     var myMap = L.map("map", {
         center: [45.52, -122.67],
-        zoom: 5
+        zoom: 6
       });
       
       // Adding a tile layer (the background map image) to our map
       // We use the addTo method to add objects to our map
       L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
         maxZoom: 18,
         id: "light-v10",
         accessToken: API_KEY
@@ -57,35 +57,42 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
         var earthquakeDepth = earthquakePoint.geometry.coordinates[2];
 
+        //console.log(earthquakeDepth);
         var color = "";
         if (earthquakeDepth < 10) {
-          color = "green";
+          color = "#84C77A";
         }
-        else if (earthquakeDepth = 10 && earthquakeDepth < 30) {
-          color = "#ABE098";
+        else if (earthquakeDepth >= 10 && earthquakeDepth < 30) {
+          color = "#BDE284";
         }
-        else if (earthquakeDepth = 30 && earthquakeDepth < 50) {
+        else if (earthquakeDepth >= 30 && earthquakeDepth < 50) {
           color = "#FFE301";
         }
-        else if (earthquakeDepth = 50 && earthquakeDepth < 70) {
+        else if (earthquakeDepth >= 50 && earthquakeDepth < 70) {
             color = "#FFA200";
         }
-        else if (earthquakeDepth = 70 && earthquakeDepth < 90) {
+        else if (earthquakeDepth >= 70 && earthquakeDepth < 90) {
         color = "#FF5E00";
         }
         else {
-          color = "red";
+          color = "#F70D1B";
         }
       
         earthquakeMarkers.push(
             L.circle([earthquakePoint.geometry.coordinates[1], earthquakePoint.geometry.coordinates[0]], 
             {
               stroke: false,
-              fillOpacity: 0.5,
-              color: "black",
+              fillOpacity: 1,
+              color: "white",
               fillColor: color,
-              radius: earthquakePoint.properties.mag * 10000
-            }).bindPopup("<h1>" +  + "</h1> <hr> <h3>Points: " +  + "</h3>").addTo(myMap));
+              radius: earthquakePoint.properties.mag * 10000,
+              paint:{
+                "circle-border": 1
+              }
+            }).bindPopup("<h4>Place: " + earthquakePoint.properties.place +
+             "</h4> <hr> <h5>Magnitude: " + earthquakePoint.properties.mag + 
+             "</h5><h5>Depth of earthquake: " + earthquakeDepth + "</h5>"
+             ).addTo(myMap));
 
         // console.log();
         //var earthquakeMarker = L.marker([earthquakePoint.geometry.coordinates[1], earthquakePoint.geometry.coordinates[0]])
@@ -95,13 +102,47 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         // .bindPopup(response[i].descriptor));
       // Add the marker to the bikeMarkers array
       //earthquakeMarkers.push(earthquakeMarker);
-    }
     
+
+    // function getColor(d) {
+    //     return d > 1000 ? '#84C77A' :
+    //            d > 500  ? '#BD0026' :
+    //            d > 200  ? '#E31A1C' :
+    //            d > 100  ? '#FC4E2A' :
+    //            d > 50   ? '#FD8D3C' :
+    //            d > 20   ? '#FEB24C' :
+    //            d > 10   ? '#FED976' :
+    //                       '#FFEDA0';
+    // }
+    
+    
+}
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) 
+{
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = ["-10-10", "10-30", "30-50", "50-70", "70-90", "+90"],
+        labels = ["#84C77A","#BDE284","#FFE301","#FFA200","#FF5E00","#F70D1B"];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) 
+    {
+        div.innerHTML +=
+            '<i style="background:' + labels[i] + '"></i><label> ' +  grades[i] + '</label><br/>';
+    }
+
+    return div;
+};
     var points = L.layerGroup(earthquakeMarkers);
 
+    legend.addTo(myMap);
     myMap.addLayer(points);
    // createMap(L.layerGroup(earthquakeMarkers));
 });
 // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
 // d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 //   createMap();
+
