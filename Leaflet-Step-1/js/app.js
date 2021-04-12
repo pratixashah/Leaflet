7d75736e5c04bf7765fe4f11f030ccecfc329835
pratixashah <shah.pratixa@gmail.com>
2021-04-12 15:12:46 -0700

    Loading Markers first then Map

--- Leaflet-Step-1/js/app.js
+++ Leaflet-Step-1/js/app.js
@@ -5,48 +5,12 @@
         zoom: 6
       });
       
-      // Adding a tile layer (the background map image) to our map
-      // We use the addTo method to add objects to our map
-      L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
-        maxZoom: 18,
-        id: "light-v10",
-        accessToken: API_KEY
-      }).addTo(myMap);
-  
-    // // Create a baseMaps object to hold the lightmap layer
-    // var baseMaps = {
-    //   "Light Map": lightmap
-    //   //"Dark Map": darkmap
-    // };
-  
-    // // Create an overlayMaps object to hold the bikeStations layer
-    // var overlayMaps = {
-    //   "Earthquake": earthquakeMarkers
-    // };
-  
-    // // Create the map object with options
-    // var map = L.map("map-id", {
-    //   center: [37.0902, 95.7129],
-    //   zoom: 2,
-    //   layers: [lightmap, earthquakeMarkers]
-    // });
-  
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    // L.control.layers(baseMaps, overlayMaps, {
-    //   collapsed: false
-    // }).addTo(map);
-//   }
-
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(response)
 {
     var data = response.features;
     //console.log(data);
 
-    //var earthquakeMarkers = [];
-    // Create a new marker cluster group
     var earthquakeMarkers = [];
-    // L.markerClusterGroup();
 
     for(var index = 0; index < data.length; index++)
     {
@@ -87,37 +51,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
               color: "black",
               fillColor: color,
               radius: earthquakePoint.properties.mag * 8000
-            //   paint:{
-            //     "circle-border": 1,
-            //     "stroke-width":1
-            //   }
             }).bindPopup("<h4>Place: " + earthquakePoint.properties.place +
              "</h4> <hr> <h5>Magnitude: " + earthquakePoint.properties.mag + 
              "</h5><h5>Depth of earthquake: " + earthquakeDepth + "</h5>"
              ).addTo(myMap));
-
-        // console.log();
-        //var earthquakeMarker = L.marker([earthquakePoint.geometry.coordinates[1], earthquakePoint.geometry.coordinates[0]])
-        // .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-  
-        // markers.addLayer(L.marker([earthquakePoint.geometry.coordinates[1], earthquakePoint.geometry.coordinates[0]]));
-        // .bindPopup(response[i].descriptor));
-      // Add the marker to the bikeMarkers array
-      //earthquakeMarkers.push(earthquakeMarker);
-    
-
-    // function getColor(d) {
-    //     return d > 1000 ? '#84C77A' :
-    //            d > 500  ? '#BD0026' :
-    //            d > 200  ? '#E31A1C' :
-    //            d > 100  ? '#FC4E2A' :
-    //            d > 50   ? '#FD8D3C' :
-    //            d > 20   ? '#FEB24C' :
-    //            d > 10   ? '#FED976' :
-    //                       '#FFEDA0';
-    // }
-    
-    
 }
 
 var legend = L.control({position: 'bottomright'});
@@ -142,9 +79,14 @@ legend.onAdd = function (map)
 
     legend.addTo(myMap);
     myMap.addLayer(points);
-   // createMap(L.layerGroup(earthquakeMarkers));
 });
-// Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
-//   createMap();
 
+// Adding a tile layer (the background map image) to our map
+      // We use the addTo method to add objects to our map
+      L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+      }).addTo(myMap);
+  
